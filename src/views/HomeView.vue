e65d9310182af5afe728274780466c0a6a604074
Aliaa Hesham <aliaa.hesham@mondia.com>
2023-09-10 13:41:06 +0300

    prettified code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,13 @@
-
 <template>
   <main>
     <PlantsList></PlantsList>
   </main>
 </template>
 
-  <script>
-  // import TheWelcome from '@/components/TheWelcome.vue'
-
-  export default {
-
-    components: {
-      PlantsList: () => import('@/components/PlantsList.vue'),
+<script>
+export default {
+  components: {
+    PlantsList: () => import('@/components/PlantsList.vue'),
   },
-  
-
-
-  }
-  </script>
+};
+</script>
